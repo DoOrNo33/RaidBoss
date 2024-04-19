8455ad58cd95f4c5e76a0ee1ec9216de45f14bf5
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-04-19 12:31:05 +0900

    Fix_Timer
    타이머 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,16 +12,17 @@ public class GameManager : MonoBehaviour
 
     public GameObject smallSeed;
     public GameObject bigSeed;
-    public Text timeTxt;
+    public Text timeTxtSec;
+    public Text timeTxtMin;
 
+    float time = 0f;
     float sec = 0f;
-    float min = 0f;
+    int min = 0;
 
 
     // ¾¾¾Ñ °ü·Ã
     float seedRegen = 0f;
     bool seedGen = false;
-    int seedAmount = 0;
 
     int[] seedsX = { 7, 14, -7, -14, 0, 0, 0, 0 };
     int[] seedsY = { 0, 0, 0, 0, 7, 14, -7, -14 };
@@ -44,17 +45,17 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        sec += Time.deltaTime;
-        timeTxt.text = sec.ToString("N0");
-        //  if (sec >= 60f)
-        //  {
-        //     min += 1f;
-        //     sec = 0f;
-        //  }
-        //  timeTxt.text = string.Format("{0:D2}:{1:D2}", min, (int)sec);
 
-        // ¾¾¾ÑÀÌ »ý¼ºµÉ ÁÂÇ¥ ¹è¿­
-        // [7, 0], [14, 0], [-7, 0], [-14, 0], [0, 7], [0, 14], [0, -7], [0, -14]
+        time += Time.deltaTime;
+        if (time >= 60f)
+        {
+            min++;
+            time = 0f;
+        }
+       
+        timeTxtSec.text = string.Format("{0:D2}:{1:D2}", min, (int)time);
+
+
 
 
 
@@ -70,6 +71,7 @@ public class GameManager : MonoBehaviour
         }
 
     }
+
     public void Plant()
     {
         int j = UnityEngine.Random.Range(0, 8);
